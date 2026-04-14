c679c04ceae1027c80ad4c39cfb1caa346dfe947
Himangshu <hksarkar@outlook.com>
2026-04-14 13:56:17 +0530

    testing frontend pipeline

--- client/src/App.js
+++ client/src/App.js
@@ -33,9 +33,7 @@ function App() {
 
   return (
     <div style={{ padding: "20px" }}>
-      <h2>User Management System Form :::::</h2>
-      <h2>User Management System Form :::::</h2>
-
+      <h2>User Management System Form ::</h2>
       <div style={{ marginBottom: "10px" }}>
         <input
           placeholder="Name"
